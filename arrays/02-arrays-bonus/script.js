d96992f96980710eccb953f3b73ba209744df29c
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-13 14:29:44 +0100

    completato bonus

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -20,9 +20,16 @@ console.log (longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+const edRemoved = teachers.indexOf("Ed");
+if (edRemoved !== -1) {
+teachers.splice(edRemoved , 1);}
+console.log (teachers);
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes("Fabio");
+console.log (isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join (", ");
+console.log (teachersString);
